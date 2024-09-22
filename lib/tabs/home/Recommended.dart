76b23407581fa0fec2df7081015d04e34513170e
Ayat Esaam <eayatessam1@gmail.com>
2024-09-22 19:42:38 +0300

    Empelemnt HomeScreen Api

---
+++ lib/tabs/home/Recommended.dart
@@ -0,0 +1,148 @@
+class Recommended {
+  Recommended({
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  Recommended.fromJson(Map<String, dynamic> json)
+      : page = json['page'],
+        results = (json['results'] as List<dynamic>?)
+            ?.map((v) => ResultsRecommended.fromJson(v))
+            .toList() ??
+            [],
+        totalPages = json['total_pages'],
+        totalResults = json['total_results'];
+
+  final num page;
+  final List<ResultsRecommended> results;
+  final num totalPages;
+  final num totalResults;
+
+  Recommended copyWith({
+    num? page,
+    List<ResultsRecommended>? results,
+    num? totalPages,
+    num? totalResults,
+  }) {
+    return Recommended(
+      page: page ?? this.page,
+      results: results ?? this.results,
+      totalPages: totalPages ?? this.totalPages,
+      totalResults: totalResults ?? this.totalResults,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'page': page,
+      'results': results.map((v) => v.toJson()).toList(),
+      'total_pages': totalPages,
+      'total_results': totalResults,
+    };
+  }
+}
+
+class ResultsRecommended {
+  ResultsRecommended({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  ResultsRecommended.fromJson(Map<String, dynamic> json)
+      : adult = json['adult'],
+        backdropPath = json['backdrop_path'],
+        genreIds = (json['genre_ids'] as List<dynamic>?)?.cast<num>() ?? [],
+        id = json['id'],
+        originalLanguage = json['original_language'],
+        originalTitle = json['original_title'],
+        overview = json['overview'],
+        popularity = json['popularity'],
+        posterPath = json['poster_path'],
+        releaseDate = json['release_date'],
+        title = json['title'],
+        video = json['video'],
+        voteAverage = json['vote_average'],
+        voteCount = json['vote_count'];
+
+  final bool adult;
+  final String backdropPath;
+  final List<num> genreIds;
+  final num id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final num popularity;
+  final String posterPath;
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final num voteAverage;
+  final num voteCount;
+
+  ResultsRecommended copyWith({
+    bool? adult,
+    String? backdropPath,
+    List<num>? genreIds,
+    num? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    num? popularity,
+    String? posterPath,
+    String? releaseDate,
+    String? title,
+    bool? video,
+    num? voteAverage,
+    num? voteCount,
+  }) {
+    return ResultsRecommended(
+      adult: adult ?? this.adult,
+      backdropPath: backdropPath ?? this.backdropPath,
+      genreIds: genreIds ?? this.genreIds,
+      id: id ?? this.id,
+      originalLanguage: originalLanguage ?? this.originalLanguage,
+      originalTitle: originalTitle ?? this.originalTitle,
+      overview: overview ?? this.overview,
+      popularity: popularity ?? this.popularity,
+      posterPath: posterPath ?? this.posterPath,
+      releaseDate: releaseDate ?? this.releaseDate,
+      title: title ?? this.title,
+      video: video ?? this.video,
+      voteAverage: voteAverage ?? this.voteAverage,
+      voteCount: voteCount ?? this.voteCount,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'adult': adult,
+      'backdrop_path': backdropPath,
+      'genre_ids': genreIds,
+      'id': id,
+      'original_language': originalLanguage,
+      'original_title': originalTitle,
+      'overview': overview,
+      'popularity': popularity,
+      'poster_path': posterPath,
+      'release_date': releaseDate,
+      'title': title,
+      'video': video,
+      'vote_average': voteAverage,
+      'vote_count': voteCount,
+    };
+  }
+}
--- lib/tabs/home/home_tap.dart
+++ lib/tabs/home/home_tap.dart
@@ -1,28 +1,88 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
 import '../../app_theme.dart';
+import 'Popular.dart';
+import 'package:http/http.dart' as http;
 
-class HomeTap extends StatelessWidget {
+import 'Recommended.dart';
+
+class HomeTap extends StatefulWidget {
   const HomeTap({super.key});
 
-  static const String routName = 'hometap';
+  static const String routeName = 'hometap';
+
+  @override
+  State<HomeTap> createState() => _HomeTapState();
+}
+
+class _HomeTapState extends State<HomeTap> {
+  late ResultsPopular resultsPopular;
+  late ResultsPopular resultsReal;
+  late ResultsRecommended resultsRecommended;
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final popularResponse = await http.get(
+        Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=efe609b0bdfb6ffffe19516c5d3bb2b6'),
+      );
+      final upcomingResponse = await http.get(
+        Uri.parse('https://api.themoviedb.org/3/movie/upcoming?api_key=efe609b0bdfb6ffffe19516c5d3bb2b6'),
+      );
+
+      if (popularResponse.statusCode == 200 && upcomingResponse.statusCode == 200) {
+        final popularData = json.decode(popularResponse.body);
+        final upcomingData = json.decode(upcomingResponse.body);
+
+        setState(() {
+          resultsPopular = ResultsPopular.fromJson(popularData['results'][0]);
+          resultsReal = ResultsPopular.fromJson(upcomingData['results'][0]); // Use appropriate class
+          resultsRecommended = ResultsRecommended.fromJson(popularData['results'][1]);
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print(e.toString());
+      // Optionally handle the error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Stack(
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
             children: [
-              Image.asset(
-                'assets/images/background.png',
+              Image.network(
+                resultsPopular.backdropPath != null && resultsPopular.backdropPath!.isEmpty
+                    ? 'https://image.tmdb.org/t/p/w500${resultsPopular.backdropPath}'
+                    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWz9tftw9qculFH1gxieWkxL6rbRk_hrXTSg&s',
                 fit: BoxFit.cover,
                 width: double.infinity,
-                height: double.infinity,
+                height: 250,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.grey,
+                    child: const Center(child: Icon(Icons.error)),
+                  );
+                },
               ),
               Container(
                 width: double.infinity,
-                height: double.infinity,
+                height: 250,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.bottomCenter,
@@ -43,11 +103,19 @@ class HomeTap extends StatelessWidget {
                   children: [
                     ClipRRect(
                       borderRadius: BorderRadius.circular(8),
-                      child: Image.asset(
-                        'assets/images/Image.png',
+                      child: Image.network(
+                        resultsPopular.posterPath != null && resultsPopular.posterPath!.isNotEmpty
+                            ? 'https://image.tmdb.org/t/p/w500${resultsPopular.posterPath}'
+                            : 'https://via.placeholder.com/100x150',
                         width: 100,
                         height: 150,
                         fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(child: Icon(Icons.error)),
+                          );
+                        },
                       ),
                     ),
                     const SizedBox(width: 16),
@@ -57,16 +125,15 @@ class HomeTap extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Dora and the lost city of gold',
+                            resultsPopular.title.isNotEmpty ? resultsPopular.title : 'Title Not Available',
                             style: Theme.of(context).textTheme.titleLarge,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          SizedBox(height: 8),
-                          Text('2019 PG-13 2h 7m',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleMedium
-                                  ?.copyWith(fontSize: 14)),
+                          const SizedBox(height: 8),
+                          Text(
+                            '${resultsPopular.releaseDate.isNotEmpty ? resultsPopular.releaseDate : 'N/A'} ${resultsPopular.voteAverage != null ? '${resultsPopular.voteAverage}/10' : ''}',
+                            style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 14),
+                          ),
                         ],
                       ),
                     ),
@@ -81,69 +148,58 @@ class HomeTap extends StatelessWidget {
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(50),
                   ),
-                  child: const Icon(
-                    Icons.play_circle,
-                    color: Colors.black,
-                    size: 50,
-                  ),
+
                 ),
               ),
             ],
           ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text('New Releases',
-                    style: Theme.of(context).textTheme.titleLarge),
-              ),
-              Expanded(
-                child: Container(
-                  color: AppTheme.graySecond,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Image.asset('assets/images/nagros.png'),
-                      Image.asset('assets/images/spiderman.png'),
-                      Image.asset('assets/images/Annabelle.png'),
-                      Image.asset('assets/images/toyStory.png'),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'New Releases',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text('Recommended',
-                    style: Theme.of(context).textTheme.titleLarge),
-              ),
-              Expanded(
-                child: Container(
-                  color: AppTheme.graySecond,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Image.asset('assets/images/spiderman.png'),
-                      Image.asset('assets/images/spiderman.png'),
-                      Image.asset('assets/images/spiderman.png'),
-                      Image.asset('assets/images/spiderman.png'),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          Container(
+            color: AppTheme.graySecond,
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: resultsReal.posterPath != null && resultsReal.posterPath!.isNotEmpty ? 1 : 0,
+              itemBuilder: (context, index) {
+                return Image.network(
+                  resultsReal.posterPath != null && resultsReal.posterPath!.isNotEmpty
+                      ? 'https://image.tmdb.org/t/p/w500${resultsReal.posterPath}'
+                      : 'https://via.placeholder.com/100x150',
+                );
+              },
+            ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Recommended',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            height: 200,
+            color: AppTheme.graySecond,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: resultsRecommended.posterPath != null && resultsRecommended.posterPath!.isNotEmpty ? 1 : 0,
+              itemBuilder: (context, index) {
+                return Image.network(
+                  resultsRecommended.posterPath != null && resultsRecommended.posterPath!.isNotEmpty
+                      ? 'https://image.tmdb.org/t/p/w500${resultsRecommended.posterPath}'
+                      : 'https://via.placeholder.com/100x150',
+                );
+
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
