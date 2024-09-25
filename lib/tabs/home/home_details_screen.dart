c7dd8dad4d19e7288e52e3b7f3f9bb324ee69a7a
Ayat Esaam <eayatessam1@gmail.com>
2024-09-25 21:56:30 +0300

    implement FireBase

--- lib/tabs/home/home_details_screen.dart
+++ lib/tabs/home/home_details_screen.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:movieapp/api/api_service.dart';
 import 'package:movieapp/app_theme.dart';
 import 'package:movieapp/tabs/home/movie_card.dart';
-import 'package:movieapp/widgets/error%20_indicator.dart';
 import 'package:movieapp/widgets/loading_indicator.dart';
+
+import '../../widgets/error _indicator.dart';
 
 class HomeDetailsScreen extends StatefulWidget {
   const HomeDetailsScreen({super.key});
@@ -29,14 +30,16 @@ class _HomeDetailsScreenState extends State<HomeDetailsScreen> {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(
+          icon: const Icon(
             Icons.arrow_back,
             color: AppTheme.white,
           ),
         ),
         title: const Text("Movies"),
       ),
-      body: ListView(
+      body: movieId == null
+          ? const Center(child: Text("Invalid movie ID"))
+          : ListView(
         children: [
           FutureBuilder(
             future: APIService.getMovieDetailsRespons(movieId!),
@@ -45,116 +48,119 @@ class _HomeDetailsScreenState extends State<HomeDetailsScreen> {
                 return const LoadingIndicator();
               } else if (snapshot.hasError) {
                 return const ErrorIndicator();
-              } else {
-                final movies = snapshot.data;
-                return Column(
-                  children: [
-                    Image.network(
-                      'https://image.tmdb.org/t/p/w500/${movies?.backdropPath}' ??
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWz9tftw9qculFH1gxieWkxL6rbRk_hrXTSg&s',
-                      height: MediaQuery.of(context).size.height * 0.25,
-                      width: double.infinity,
-                      fit: BoxFit.fill,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 22, vertical: 14),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            movies?.originalTitle ?? 'No Title',
-                            style: Theme.of(context).textTheme.titleLarge,
+              } else if (!snapshot.hasData || snapshot.data == null) {
+                return const Center(
+                    child: Text("No movie details available."));
+              }
+
+              final movies = snapshot.data;
+              return Column(
+                children: [
+                  Image.network(
+                    movies?.backdropPath != null
+                        ? 'https://image.tmdb.org/t/p/w500/${movies?.backdropPath}'
+                        : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWz9tftw9qculFH1gxieWkxL6rbRk_hrXTSg&s',
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 22, vertical: 14),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          movies?.originalTitle ?? 'No Title',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Text(
+                          movies?.releaseDate ?? '',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(
+                            fontSize: 10,
                           ),
-                          Text(
-                            movies?.releaseDate ?? '',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge!
-                                .copyWith(
-                                  fontSize: 10,
-                                ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: [
-                              Image.network(
-                                'https://image.tmdb.org/t/p/w500/${movies?.posterPath}' ??
-                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWz9tftw9qculFH1gxieWkxL6rbRk_hrXTSg&s',
-                                fit: BoxFit.fill,
-                                height:
-                                    MediaQuery.of(context).size.height * 0.22,
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsetsDirectional.only(start: 10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    ActionChip(
-                                      backgroundColor: AppTheme.black,
-                                      label: Text(
-                                        movies?.belongsToCollection?.name ??
-                                            'No Name',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleSmall!
-                                            .copyWith(
-                                              fontWeight: FontWeight.bold,
-                                              color: AppTheme.white,
-                                            ),
-                                      ),
-                                      onPressed: () {},
-                                      side: const BorderSide(
-                                        width: 2,
-                                        color: AppTheme.gold,
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 18,
-                                    ),
-                                    Text(
-                                      formatText(
-                                          movies?.overview ??
-                                              'Overview not available.',
-                                          30),
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Image.network(
+                              movies?.posterPath != null
+                                  ? 'https://image.tmdb.org/t/p/w500/${movies?.posterPath}'
+                                  : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWz9tftw9qculFH1gxieWkxL6rbRk_hrXTSg&s',
+                              fit: BoxFit.fill,
+                              height:
+                              MediaQuery.of(context).size.height *
+                                  0.22,
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                  start: 10),
+                              child: Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                                children: [
+                                  ActionChip(
+                                    backgroundColor: AppTheme.black,
+                                    label: Text(
+                                      movies?.belongsToCollection?.name ??
+                                          'No Collection',
                                       style: Theme.of(context)
                                           .textTheme
-                                          .titleSmall,
+                                          .titleSmall!
+                                          .copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        color: AppTheme.white,
+                                      ),
                                     ),
-                                    const SizedBox(
-                                      height: 18,
+                                    onPressed: () {},
+                                    side: const BorderSide(
+                                      width: 2,
+                                      color: AppTheme.gold,
                                     ),
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.star,
-                                          color: AppTheme.gold,
-                                        ),
-                                        Text(
-                                          movies?.voteAverage.toString() ?? '',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleLarge,
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ),
+                                  ),
+                                  const SizedBox(height: 18),
+                                  Text(
+                                    formatText(
+                                        movies?.overview ??
+                                            'Overview not available.',
+                                        30),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall,
+                                  ),
+                                  const SizedBox(height: 18),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.star,
+                                        color: AppTheme.gold,
+                                      ),
+                                      Text(
+                                        movies?.voteAverage
+                                            .toString() ??
+                                            'N/A',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleLarge,
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    MovieCard(
-                      movieId: movieId!,
-                    ),
-                  ],
-                );
-              }
+                  ),
+                  MovieCard(
+                    movieId: movieId!,
+                  ),
+                ],
+              );
             },
           ),
         ],
